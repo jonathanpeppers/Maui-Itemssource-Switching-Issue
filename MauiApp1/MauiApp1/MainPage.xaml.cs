43af9ec6e1698f2fe87499c803dd8251f849a833
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-08-17 11:56:23 -0500

    Log how many `CheckBox` alive

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -1,28 +1,45 @@
 ﻿
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 
 namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
+        List<WeakReference> references = new List<WeakReference>();
  
 
         public MainPage()
         {
             InitializeComponent();
 
+            listView.ItemTemplate = new DataTemplate(() =>
+            {
+                var cell = new ViewCell { View = new Label() };
+                references.Add(new(cell.View));
+                return cell;
+            });
+        }
+
+        void Log()
+        {
+            Debug.WriteLine($"CheckBox's alive: {references.Count(l => l.IsAlive)}");
         }
 
         private void ItemSource_One_Button_Clicked(object sender, EventArgs e)
         {
+			GC.Collect();
+			Log();
             listView.ItemsSource = employeeInfoRepository.EmployeesInfo1;
             
         }
 
         private void ItemSource_Two_Button_Clicked(object sender, EventArgs e)
         {
-            listView.ItemsSource = employeeInfoRepository.EmployeesInfo2;
+			GC.Collect();
+			Log();
+			listView.ItemsSource = employeeInfoRepository.EmployeesInfo2;
         }
     }
 }
